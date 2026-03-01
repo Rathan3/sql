d7bb1afe8689b46e19784429b2133df0ce131a0e
Rathan3 <rathantejaabc@gmail.com>
2026-03-01 23:28:55 +0530

    Q6

--- Group_By.sql
+++ Group_By.sql
@@ -11,4 +11,25 @@ b as (select user_id, count(*) as fc from famous group by user_id)
 select a.user_id , 
 case when b.user_id is null then 0 else round((b.fc/(select count(*) from a))*100,2) end as famous_percentage 
 from a left join b 
-on a.user_id = b.user_id
+on a.user_id = b.user_id;
+
+-- Given a list of projects and employees mapped to each project, calculate by the amount of project budget allocated to each employee. 
+-- The output should include the project title and the project budget rounded to the closest integer. 
+-- Order your list by projects with the highest budget per employee first.
+CREATE TABLE ms_projects(id int, title varchar(15), budget int);
+INSERT INTO ms_projects VALUES (1, 'Project1',  29498),(2, 'Project2',  32487),(3, 'Project3',  43909),(4, 'Project4',  15776),(5, 'Project5',  36268),(6, 'Project6',  41611),(7, 'Project7',  34003),(8, 'Project8',  49284),(9, 'Project9',  32341),(10, 'Project10',    47587),(11, 'Project11',    11705),(12, 'Project12',    10468),(13, 'Project13',    43238),(14, 'Project14',    30014),(15, 'Project15',    48116),(16, 'Project16',    19922),(17, 'Project17',    19061),(18, 'Project18',    10302),(19, 'Project19',    44986),(20, 'Project20',    19497);
+CREATE TABLE ms_emp_projects(emp_id int, project_id int);
+INSERT INTO ms_emp_projects VALUES (10592,  1),(10593,  2),(10594,  3),(10595,  4),(10596,  5),(10597,  6),(10598,  7),(10599,  8),(10600,  9),(10601,  10),(10602, 11),(10603, 12),(10604, 13),(10605, 14),(10606, 15),(10607, 16),(10608, 17),(10609, 18),(10610, 19),(10611, 20);
+
+with a as(select project_id , count(*) as num_emp from ms_emp_projects group by project_id)
+select p.title, round(p.budget/a.num_emp,0) as budget_per_emp from ms_projects p inner join a on p.id = a.project_id order by budget_per_emp desc;
+
+-- Find the total number of available beds per hosts' nationality.
+-- Output the nationality along with the corresponding total number of available beds. 
+-- Sort records by the total available beds in descending order.
+CREATE TABLE airbnb_apartments(host_id int,apartment_id varchar(5),apartment_type varchar(10),n_beds int,n_bedrooms int,country varchar(20),city varchar(20));
+INSERT INTO airbnb_apartments VALUES(0,'A1','Room',1,1,'USA','NewYork'),(0,'A2','Room',1,1,'USA','NewJersey'),(0,'A3','Room',1,1,'USA','NewJersey'),(1,'A4','Apartment',2,1,'USA','Houston'),(1,'A5','Apartment',2,1,'USA','LasVegas'),(3,'A7','Penthouse',3,3,'China','Tianjin'),(3,'A8','Penthouse',5,5,'China','Beijing'),(4,'A9','Apartment',2,1,'Mali','Bamako'),(5,'A10','Room',3,1,'Mali','Segou');
+CREATE TABLE airbnb_hosts(host_id int,nationality  varchar(15),gender varchar(5),age int);
+INSERT INTO airbnb_hosts  VALUES(0,'USA','M',28),(1,'USA','F',29),(2,'China','F',31),(3,'China','M',24),(4,'Mali','M',30),(5,'Mali','F',30);
+
+select b.nationality, sum(n_beds) as bph from airbnb_apartments a inner join airbnb_hosts b on a.host_id = b.host_id group by b.nationality order by bph desc;
--- Window_Function.sql
+++ Window_Function.sql
@@ -19,3 +19,17 @@ select * from sf_transactions;
 with a as(select date_format(created_at,"%Y-%m") as mon, sum(value) as m_r from sf_transactions group by mon ),
 b as (select mon,m_r, lag(m_r) over (order by mon ) as pm_r from a order by mon)
 select mon as month,m_r as total_revenue,round(((m_r-pm_r)/pm_r)*100,2) as percentage_change from b 
+
+-- To create a naïve forecast for "distance per dollar" (defined as distance_to_travel/monetary_cost),
+-- first sum the "distance to travel" and "monetary cost" values monthly.
+-- This gives the actual value for the current month. For the forecasted value, use the previous month's value.
+-- After obtaining both actual and forecasted values,
+-- calculate the root mean squared error (RMSE) using the formula RMSE = sqrt(mean(square(actual - forecast))).
+-- Report the RMSE rounded to two decimal places.
+CREATE TABLE uber_request_logs(request_id int, request_date datetime, request_status varchar(10), distance_to_travel float, monetary_cost float, driver_to_client_distance float);
+INSERT INTO uber_request_logs VALUES (1,'2020-01-09','success', 70.59, 6.56,14.36), (2,'2020-01-24','success', 93.36, 22.68,19.9), (3,'2020-02-08','fail', 51.24, 11.39,21.32), (4,'2020-02-23','success', 61.58,8.04,44.26), (5,'2020-03-09','success', 25.04,7.19,1.74), (6,'2020-03-24','fail', 45.57, 4.68,24.19), (7,'2020-04-08','success', 24.45,12.69,15.91), (8,'2020-04-23','success', 48.22,11.2,48.82), (9,'2020-05-08','success', 56.63,4.04,16.08), (10,'2020-05-23','fail', 19.03,16.65,11.22), (11,'2020-06-07','fail', 81,6.56,26.6), (12,'2020-06-22','fail', 21.32,8.86,28.57), (13,'2020-07-07','fail', 14.74,17.76,19.33), (14,'2020-07-22','success',66.73,13.68,14.07), (15,'2020-08-06','success',32.98,16.17,25.34), (16,'2020-08-21','success',46.49,1.84,41.9), (17,'2020-09-05','fail', 45.98,12.2,2.46), (18,'2020-09-20','success',3.14,24.8,36.6), (19,'2020-10-05','success',75.33,23.04,29.99), (20,'2020-10-20','success', 53.76,22.94,18.74);
+
+ with a as(select date_format(request_date,'%Y-%m') as mon,sum(distance_to_travel)/sum(monetary_cost) as d_p_d
+ from uber_request_logs group by mon),
+ b as (select mon, d_p_d, lag(d_p_d) over (order by mon ) as p_p_d from a)
+ select round(sqrt(avg(pow(d_p_d-p_p_d,2))),2) as RMSE from b
